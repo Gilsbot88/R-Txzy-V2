1744f2fa7ea45540815d5346a82dbd35b0334bdd
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-18 21:57:09 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,9 +3,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085730903853 [AlyaXd]
-├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
+├ GoPay: 081318910218 [nomornya]
+├ Dana: 083136890848[nomornya]
+├ Pulsa: 083136890848 [nomornya]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
